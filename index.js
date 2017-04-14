583872bde5ed588ac2d36b5417620214bdd74b6e
Igor Chernyy <igor.chernyy@gmail.com>
2017-04-14 11:34:41 -0400

    More clean up.

--- index.js
+++ index.js
@@ -1,67 +1,63 @@
 'use strict';
 
 const elasticsearch = require('elasticsearch');
-const winston = require('winston');
+const httpAWSES = require('http-aws-es');
 const AWS = require('aws-sdk');
 const Q = require('q');
 
 class ESServer {
+  constructor(host, awsRegion, log) {
+    const credentials = new AWS.EnvironmentCredentials('AWS');
+    const region = (awsRegion || process.env.AWS_DEFAULT_REGION);
+    this.client = new elasticsearch.Client({
+      connectionClass: httpAWSES,
+      host,
+      amazonES: {
+        credentials,
+        region,
+      },
+      log,
+    });
+  }
 
-    constructor(host, awsRegion, log) {
-        const credentials = new AWS.EnvironmentCredentials('AWS');
-        const region = (awsRegion || process.env.AWS_DEFAULT_REGION);
-        this.client = new elasticsearch.Client({
-            connectionClass: require('http-aws-es'),
-            host,
-            amazonES: {
-                credentials,
-                region
-            },
-            log
-        });
-    }
+  doesIndexExist(index) {
+    const deferred = Q.defer();
+    const params = { index };
+    this.client.indices.exists(params)
+        .then(result => deferred.resolve(result), err => deferred.reject(err));
+    return deferred.promise;
+  }
 
-    doesIndexExist(index) {
-        const deferred = Q.defer();
-        const params = { index };
-        this.client.indices.exists(params).
-        then(result => deferred.resolve(result), err => deferred.reject(err));
-        return deferred.promise;
-    }
+  updateDocument(index, routing, type, id, document) {
+    const deferred = Q.defer();
+    const params = { index, type, id, routing, body: { doc: document } };
+    this.client.update(params)
+        .then(result => deferred.resolve(result), err => deferred.reject(err));
+    return deferred.promise;
+  }
 
-    updateDocument(index, routing, type, id, document) {
-        winston.debug('Image Record - es update:', { index, routing, type, id });
-        const deferred = Q.defer();
-        const params = { index, type, id, routing, body: { doc: document } };
-        this.client.update(params).
-        then(result => deferred.resolve(result), err => deferred.reject(err));
-        return deferred.promise;
-    }
+  deleteDocument(index, routing, type, id) {
+    const deferred = Q.defer();
+    const params = { index, type, id, routing };
+    this.client.delete(params)
+        .then(result => deferred.resolve(result), err => deferred.reject(err));
+    return deferred.promise;
+  }
 
-    deleteDocument(index, routing, type, id) {
-        winston.debug('Image Record - es delete:', { index, routing, type, id });
-        const deferred = Q.defer();
-        const params = { index, type, id, routing };
-        this.client.delete(params).
-        then(result => deferred.resolve(result), err => deferred.reject(err));
-        return deferred.promise;
-    }
+  createDocument(index, routing, type, id, document) {
+    const deferred = Q.defer();
+    const params = { body: document, index, type, routing, id };
+    this.client.index(params)
+        .then(result => deferred.resolve(result), err => deferred.reject(err));
+    return deferred.promise;
+  }
 
-    createDocument(index, routing, type, id, document) {
-        winston.debug('Image Record - es create:', { index, routing, type, id });
-        const deferred = Q.defer();
-        const params = { body: document, index, type, routing, id };
-        this.client.index(params).
-        then(result => deferred.resolve(result), err => deferred.reject(err));
-        return deferred.promise;
-    }
-
-    ping() {
-        const deferred = Q.defer();
-        this.client.ping({ requestTimeout: 30000, hello: 'elasticsearch' }).
-        then(() => deferred.resolve(), err => deferred.reject(err));
-        return deferred.promise;
-    }
+  ping() {
+    const deferred = Q.defer();
+    this.client.ping({ requestTimeout: 30000, hello: 'elasticsearch' })
+        .then(() => deferred.resolve(), err => deferred.reject(err));
+    return deferred.promise;
+  }
 }
 
 module.exports = ESServer;
